d5869c3d4923d33cd4285ba0ca583e90b9a5da44
Santiago Molina Molina <canopus44@gmail.com>
2026-09-08 16:19:30 -0500

    fix: self-host TMDB logo to avoid external image block

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <a href="https://www.themoviedb.org" target="_blank" rel="noreferrer">
           <img
             className="tmdb-logo"
-            src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228ca3db904c0dca977aed7a0e.svg"
+            src="tmdb-logo.svg"
             alt="The Movie Database (TMDB)"
           />
         </a>
